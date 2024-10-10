c57a763b5a1fe64b369eb9be0e65807dd147cd9f
I504428 <gabriel.azambuja@sap.com>
2024-10-10 18:35:56 -0300

    feat: add routes for store

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,38 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
+
+const DashboardLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { storeId: string };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <>
+      <div>This will be a nav var</div>
+      {children}
+    </>
+  );
+};
+
+export default DashboardLayout;
