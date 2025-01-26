d1d03bd5125840babbeb96a07184ce16cdb3fc2a
I504428 <gabriel.azambuja@sap.com>
2025-01-26 00:47:43 -0300

    feat: adding stripe for payments

---
+++ lib/stripe.ts
@@ -0,0 +1,7 @@
+import Stripe from "stripe";
+import { env } from "process";
+
+export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
+  apiVersion: "2024-12-18.acacia",
+  typescript: true,
+});
